d505af456474f22d4de70243c8c4d9fb467cbb65
Chris Primes <chris@chrisprimes.com>
2023-02-06 11:22:07 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [data, setData] = useState(false)
+
+  const apiFetch = async (url) => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(json);
+  }
+
+  useEffect(() => {
+    apiFetch("https://www.rochestermfa.org/api/rmfa/v1/collection/all");
+  }, []);
+
+  const renderList = () => {
+    let list = [];
+    data.forEach(element => {
+      list.push(<p key={element.id}>{element.collection_number}. {element.artist}, <span class="italic">{element.title}</span>, {element.art_medium}</p>)
+    });
+
+    return list;
+  };
+
+  return (
+    <div>
+      {data && renderList()}
+    </div>
+  );
+}
+
+export default App;
