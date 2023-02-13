feeee4b781253af87635c21c3894347f59190476
Chris Primes <chris@chrisprimes.com>
2023-02-12 19:02:43 -0500

    Error handling

--- src/Labels.js
+++ src/Labels.js
@@ -4,21 +4,36 @@ import './App.css';
 
 const Labels = () => {
     const [collectionNumberInput, setCollectionNumberInput] = useState('');
+    const [error, setError] = useState(false);
 
     const apiFetch = async (url) => {
         const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Error fetching data fron API.');
+        }
+
         const body = await response.text();
 
         return Buffer.from(body, 'base64');
     }
 
     const printLabels = async () => {
-        let userInput = collectionNumberInput.trim();
-        if(userInput === '') {
+        setError(false);
+
+        let userInput = collectionNumberInput.trim().split(',');
+
+        if (userInput.length === 1 && userInput[0] === '') {
             userInput = '-1'
         }
 
-        const data = await apiFetch("http://localhost:8080/reports/collectionAvery5160?collectionNumber=" + userInput)
+        let data;
+        try {
+            data = await apiFetch("http://localhost:8080/reports/collectionAvery5160?collectionNumber=" + userInput);
+        } catch (e) {
+            setError("Error generating report. Check that your input is valid.");
+            return;
+        }
 
         const blob = new Blob([data], { type: 'application/pdf' });
         const url = URL.createObjectURL(blob);
@@ -29,6 +44,8 @@ const Labels = () => {
     return (
         <div>
             <h1>Collection Labels</h1>
+
+            {error && <div className="error">{error}</div>}
 
             <textarea rows="10" cols="50" value={collectionNumberInput} onChange={(event) => setCollectionNumberInput(event.target.value)}>
             </textarea>
