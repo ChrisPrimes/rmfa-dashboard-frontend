ff8501fab6664663f197172ed5d7d8ffb13d142d
Chris Primes <chris@chrisprimes.com>
2023-02-17 23:44:23 -0500

    Fix merge issue

--- src/App.js
+++ src/App.js
@@ -17,10 +17,6 @@ import { Logout } from './Logout'
 import './style.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-  useEffect(() => {
-    apiFetch("https://www.rochestermfa.org/api/rmfa/v1/collection/list?collection_number=-1");
-  }, []);
-
 const App = () => {
   return (
     <Router>
